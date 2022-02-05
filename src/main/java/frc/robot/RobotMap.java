3169be66f8aa56b2380d28b6312672eb0882f7e7
CalebOverbeek <caleb.overbeek@outlook.com>
2022-02-05 10:49:58 -0500

    Add values to the left wall path for Auton to prepare it for testing

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -2,6 +2,9 @@ package frc.robot;
 
 public class RobotMap {
 
+    //timeout in milliseconds for the CTRE config methods
+    public static final int TIMEOUT_MS = 30;
+
     /**
      * constant for PCM location on the CAN bus
      */
@@ -23,9 +26,6 @@ public class RobotMap {
         //constants for gearbox solenoid (on the PCM)
         public static final int DOUBLESOLENOID_LOW_GEAR_PORT = 4; 
         public static final int DOUBLESOLENOID_HIGH_GEAR_PORT = 5;
-
-        //timeout in milliseconds for the CTRE config methods
-        public static final int TIMEOUT_MS = 30;
     }
 
     /**
@@ -66,9 +66,6 @@ public class RobotMap {
         public static final double FEEDING_SPEED = 0.5;
         public static final double TRAJECTORY_MOTOR_SPEED = 0.25;
         public static final double EXPEL_SPEED = 0.25;
-
-        //the launcher timeout for running confing methods
-        public static final int CONFIG_TIMEOUT_MS = 30;
 
         public static final double POSITIVE_TURRET_ROTATION_SPEED = 0.25;
         public static final double NEGATIVE_TURRET_ROTATION_SPEED = -0.25;
@@ -150,7 +147,7 @@ public class RobotMap {
         public static final double INCHES_TO_ENCODER_TICKS_LOWGEAR = 2048 / (18.85 / 15);
         public static final double INCHES_TO_ENCODER_TICKS_HIGHGEAR = 2048 / (18.85 / 7.92);
 
-        public static final double PLACEHOLDER_VALUE_SPEED = 1;
+        public static final double PLACEHOLDER_VALUE_SPEED = 0.5;
         public static final double PLACEHOLDER_VALUE_DISTANCE = 1;
         public static final double PLACEHOLDER_VALUE_ANGLE_CLOCKWISE = 1;
         public static final double PLACEHOLDER_VALUE_ANGLE_COUNTERCLOCKWISE = -1;
