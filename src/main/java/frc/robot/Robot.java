eea881e2ad10e9094c8831e0c7c03c70271fdb7a
sam-halliday <samuel.l.halliday@gmail.com>
2022-02-28 16:13:24 -0800

    removed another shuffleboard init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -80,7 +80,6 @@ public class Robot extends TimedRobot {
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
-    m_shuffleboard.init();
     
   }
 
