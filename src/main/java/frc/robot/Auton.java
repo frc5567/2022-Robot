388213c38488abdb52f4c5470f3c56eaeb0e86ad
EmmaOverbeek <emmaroverbeek@gmail.com>
2022-03-05 10:25:04 -0500

    Auton testing fixes from Thursday

--- src/main/java/frc/robot/Auton.java
+++ src/main/java/frc/robot/Auton.java
@@ -115,7 +115,7 @@ public class Auton{
         //calls the limelight periodic method in order to update the network tables every cycle
         m_limelightVision.periodic();
         //calls the intake periodic method for automatic indexing
-        m_intake.periodic();
+        //m_intake.periodic();
 
         m_currentRightEncoderTicks = m_drivetrain.getRightDriveEncoderPosition();
         m_currentLeftEncoderTicks = m_drivetrain.getLeftDriveEncoderPosition();
