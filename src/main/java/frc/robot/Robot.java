ad6a98556b70cccb850c67b3728a1f39779d1b9c
EmmaOverbeek <emmaroverbeek@gmail.com>
2022-03-10 16:42:08 -0500

    Deleted unused code from Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -15,11 +15,6 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * project.
  */
 public class Robot extends TimedRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
-  
   private PilotController m_pilotController;
   private CopilotController m_copilotController;
   private Auton m_auton;
@@ -37,9 +32,6 @@ public class Robot extends TimedRobot {
    */
   @Override
   public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
     
     m_intake = new Intake();
     m_limelightVision = new LimelightVision();
@@ -77,26 +69,13 @@ public class Robot extends TimedRobot {
   @Override
   public void autonomousInit() {
     m_auton.init();
-    m_autoSelected = m_chooser.getSelected();
-    // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-    
   }
 
   /** This function is called periodically during autonomous. */
   @Override
   public void autonomousPeriodic() {
     m_auton.periodic();
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
     }
-  }
 
   /** This function is called once when teleop is enabled. */
   @Override
