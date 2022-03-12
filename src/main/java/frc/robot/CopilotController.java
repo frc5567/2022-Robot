c766340271e1ada5c49cd23794128e1c17064095
HannahStrimpel <h4rescue@gmail.com>
2022-03-11 19:22:13 -0500

    Instantiated the GamePad in CopilotController and add a robotMap value to hold a port number for this.

--- src/main/java/frc/robot/CopilotController.java
+++ src/main/java/frc/robot/CopilotController.java
@@ -34,6 +34,7 @@ public class CopilotController {
         m_shuffleboard = shuffleboard;
         m_limelight = limelight;
         
+        m_gamePad = new GamePad(RobotMap.GamePadConstants.GAMEPAD_PORT);
         m_controller = new XboxController(RobotMap.CopilotControllerConstants.COPILOT_CONTROLLER_PORT);
         //m_gamePad = new GamePad(RobotMap.CopilotControllerConstants.GAMEPAD_PORT);
 
